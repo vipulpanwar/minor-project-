5af1872ecb22be3830893b4c36102772d34bf370
unknown <anuj.talwar.12@gmail.com>
2021-02-17 23:37:37 +0530

    home

--- src/containers/Home/Card.js
+++ src/containers/Home/Card.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import { Link } from "react-router-dom";
 import './home.css';
 import Background1 from './background-1.svg';
-import Content from './content-to-be-replaced.svg';
+import Content from './images/content-to-be-replaced.svg';
+import Ibutton from './images/ibutton.svg';
 
 function Card () {
   return(
@@ -12,7 +13,11 @@ function Card () {
       <img className='card-logo' src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-icon-png-transparent-background-osteopathy-16.png" alt="google logo icon png transparent background osteopathy" />
       <br /><br /><br /><br /><br />
       <div className='card-container'>
-        <img className='content-tbr' width='90%' src={Content} />
+        <h1 className="job-title">Android Developer</h1>
+        <img src={Ibutton} className="i-button"/>
+        <p className='job-details'>Full Time | 2.3 LPA | 12 Jan’20</p>
+        <p className='students-applied'>Students Applied</p>
+        <p className='students-number'>42</p> 
       </div>
     </div>
   );
