ed8bb0684d462ce4026af886c65468f9491f5f99
Unnat Malkoti <unnatmalkoti@gmail.com>
2021-02-17 22:31:15 +0530

    folder structure change

---
+++ src/components/AppliedStudents/StudentsHeader/StudentsHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './StudentsHeader.module.css';
+import logo from '../../../assets/images/ensvee-logo-white.svg';
+import backIcon from '../../../assets/icons/back.svg';
+
+export default (props)=>{
+    return(<header className={styles.StudentsHeader}>
+        <button className={styles.BackButton}><img src={backIcon}></img></button>
+        <img src={logo} className={styles.Logo}></img>
+        <h2 className={styles.Title}>{props.title}</h2>
+        <h4 className={styles.SubTitle}>{props.subTitle}</h4>        
+    </header>)
+}
--- src/containers/AppliedStudents/AppliedStudents.js
+++ src/containers/AppliedStudents/AppliedStudents.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import StudentsHeader from '../../components/AppliedStudents/StudentsHeader/StudentsHeader';
 
 class Home extends Component{
     render(){
         return(<div>
+                <StudentsHeader title="42 Students Applied" subTitle="Android Developer"/>
                 <h1>Applied Students Page</h1>
                 <Link to="/" > Home</Link>
             </div>);
