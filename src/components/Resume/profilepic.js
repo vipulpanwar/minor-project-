ead8cc105e271d2e29ecd14e0ac1911abf0c9415
unknown <anuj.talwar.12@gmail.com>
2021-02-19 21:22:00 +0530

    resume css

--- src/components/Resume/profilepic.js
+++ src/components/Resume/profilepic.js
@@ -9,12 +9,14 @@ export default  ()=>{
               <div className="profilepic">
                 <img className="profileimg" style={{marginLeft:'-8px'}} src={Profileimg} />
               </div>
-              <p className="applicant-name">Anuj Talwar</p>
-              <p className="applicant-details">Electronics and Communication Technologies - Bachelors in Technologies (B.tech)  |  Visakhapatnam</p>
-              <p className="applicant-bio">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type s
-              </p>
-              <div className="rating-box"><p className='rating-text'><span style={{color:"#898989"}}>Rating:</span> 4.5</p></div>
+              <div className="applicant-info">
+                <p className="applicant-name">Anuj Talwar</p>
+                <p className="applicant-details">Electronics and Communication Technologies - Bachelors in Technologies (B.tech)  |  Visakhapatnam</p>
+                <p className="applicant-bio">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type s
+                </p>
+                <div className="rating-box"><p className='rating-text'><span style={{color:"#898989"}}>Rating:</span> 4.5</p></div>
+              </div>
             </div>
     )
 }
