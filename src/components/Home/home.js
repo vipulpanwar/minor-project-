228d10a6d4e2536e092492d837a267bcc799e7b8
Unnat Malkoti <unnatmalkoti@gmail.com>
2021-03-27 14:19:39 +0530

    some more fixes

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,7 +2,7 @@ import React, { createRef, Fragment} from 'react';
 import {connect} from 'react-redux';
 import {Logout as logoutAction} from '../../store/actions/auth';
 import {FetchAllJobs as FetchJobsAction } from '../../store/actions/jobs';
-import {Route, withRouter} from 'react-router-dom';
+import {Route, Switch, withRouter} from 'react-router-dom';
 
 
 import './home.css';
@@ -13,65 +13,34 @@ import Loader from '../shared/ui/Loader/Loader';
 import BottomNav from './BottomNav';
 import {ModalWithHeader} from '../shared/ui/Modal/Modal';
 import NewJobForm from '../NewJobForm/NewJobForm';
-import {db} from '../../firebase';
+
+import JobList from './JobList';
+import Profile from '../Profile/Profile';
 
 class Home extends React.Component{
-  state = {
-    jobs:[],
-    loading:true,
-  }
-
-  async componentDidMount(){
-
-    let jobsDocs = await db.collection('jobs').get();
-    let jobs = [];
-    jobsDocs.forEach(jobDoc=>{
-      let job = jobDoc.data();
-      job.id = jobDoc.id;
-      jobs.push(job);
-    })
-
-    this.setState({jobs, loading:false})
-    console.log('Component did mount');
-    // this.props.getJobs();
-  }
+  
   modalCloseHandler = ()=>{
     this.props.history.push("/")
   }
     render(){
         return(
-        <div className="home-container">
-            <div className='home-top-bar'>
-              <img className='logo' src= {Logo} />
-              <Button className='log-out' clicked={this.props.logout} width="127px" height="51px" style={{position:'absolute', right:'29px', top:'30px'}}>Log Out</Button>
-              <p className='job-postings'>Job Postings</p>
-            </div>
-            { this.state.loading ? 
-            <div className="central-loader"><Loader/></div>
-            
-            : 
-            <Fragment>
-              <Slider jobs={this.state.jobs}/>
-            </Fragment>}
-            
-            <Route exact path={`/new`}  >
-              <ModalWithHeader title="Create New Job Posting" show={true} closeHandler={this.modalCloseHandler}>
-                  <NewJobForm close={this.modalCloseHandler}/>
-              </ModalWithHeader>
+        <div>
+          <Switch>          
+            <Route path="/profile" component={Profile}/>
+            <Route path="" >
+              <JobList />
+              <Route exact path={`/new`}  >
+                <ModalWithHeader title="Create New Job Posting" show={true} closeHandler={this.modalCloseHandler}>
+                    <NewJobForm close={this.modalCloseHandler}/>
+                </ModalWithHeader>
+              </Route>
             </Route>
-            <BottomNav/>
+          </Switch>
+        
+          <BottomNav/>
         </div>);
     }
 }
 
 
-const mapDispatchToProps = (dispatch) => ({
-    logout : ()=> dispatch(logoutAction()),
-    getJobs: ()=> dispatch(FetchJobsAction()),
-})
-
-const mapStateToProps = (state)=>({
-  jobsState: state.jobs
-})
-
-export default connect(mapStateToProps, mapDispatchToProps) (withRouter(Home));
+export default withRouter(Home);
