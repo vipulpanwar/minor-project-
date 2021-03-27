cc5b5c82958a8548af33f302df2d453eec8eff4f
unknown <anuj.talwar.12@gmail.com>
2021-03-27 16:19:54 +0530

    min qual

--- src/components/Home/Qualification.js
+++ src/components/Home/Qualification.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
 import styles from "./minimumqualifications.module.css"
 import xth from './images/xth.svg'
+import xiith from './images/xiith.svg'
+import bachelors from './images/bachelors.svg'
+import masters from './images/masters.svg'
 
 export default class Qualification extends Component {
     render() {
-        console.log(this.props.degree)
         return (
             <div className={styles.qualificationcontainer}>
                 <div style={{alignItems:'right'}}>
-                    <img src={xth} style={{float:'left', paddingRight:'14px'}}/> 
+                    {(this.props.degree=="xiith" || this.props.degree=='diploma') && <img src={xiith} style={{float:'left', paddingRight:'14px'}}/>}
+                    {this.props.degree=="xth" && <img src={xth} style={{float:'left', paddingRight:'14px'}}/>}
+                    {(this.props.degree=="bachelors" || this.props.degree=='phd') && <img src={bachelors} style={{float:'left', paddingRight:'14px'}}/>}
+                    {this.props.degree=="masters" && <img src={masters} style={{float:'left', paddingRight:'14px'}}/>}
                     <div className={styles.degreeContainer}>
                         <div className={styles.degreeTitle}>{this.props.degree}</div>
                         <div className={styles.percent}>{this.props.percent}%</div>
